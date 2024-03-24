776e46a9fcad8599a1113b8afa23157d4f0235f7
El Dabbabi Mouine <mouine.eldabbabi@insat.ucar.tn>
2024-03-24 17:15:56 +0100

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ikrili</title>
+</head>
+<body>
+  <h1>Ikrili</h1>
+  <br/>
+  <textarea >
+  <button>submit </button>
+</body>
+</html>
+<?php 
+
+echo "Our first Website";
+
+?>
