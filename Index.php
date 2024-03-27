feee5ecb69028b2a6dca512917ae312504332728
El Dabbabi Mouine <mouine.eldabbabi@insat.ucar.tn>
2024-03-27 07:17:43 +0100

    I start making the login page

--- Index.php
+++ Index.php
@@ -4,16 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ikrili</title>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
   <h1>Ikrili</h1>
-  <br/>
-  <textarea >
-  <button>submit </button>
+  <form action="index.php" method="post">
+  <label>Username</label><br/>
+  <input type="text" name="username"><br/>
+  <label>Password</label><br/>
+  <input type="password" name="password"><br/>
+  <input type="submit" value="Login">
+</form>
 </body>
 </html>
 <?php 
 
-echo "Our first Website";
-
+echo "{$_POST["username"]} <br>" ;
+echo "{$_POST["password"]} <br> " ;
 ?>
